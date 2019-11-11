6afea84c0ee5d224a6d55a2603223066f286a522
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2019-11-12 02:28:10 +0330

    filtering mecanism

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: true }));
-app.use([shopRoute, adminRoute]);
+app.use(shopRoute);
+// add filtering mecanism
+app.use("/admin", adminRoute);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not Found</h1>");
