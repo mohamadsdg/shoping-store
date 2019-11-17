79e9174aa053f2a8051cf09ad65d3a877c9f4d22
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2019-11-18 00:42:54 +0330

    work with query string | functionality edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,44 @@
 const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     title: "Add Product",
-    path: "admin/add-product"
+    path: "admin/add-product",
+    editMode: false
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
-  const product = new Product(title, imageUrl, price, description);
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
 
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  const prodId = req.params.productId;
+  !editMode && res.redirect("/");
+
+  Product.findByIndex(prodId, (err, product) => {
+    !err &&
+      res.render("admin/edit-product", {
+        title: "Edit Product",
+        path: "admin/edit-product",
+        editMode: editMode,
+        product
+      });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, imageUrl, price, description } = req.body;
+  let id = productId;
+  const product = new Product(id, title, imageUrl, price, description);
+  product.save();
+
+  res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,12 @@ router.get("/add-product", adminController.getAddProduct);
 // admin/add-product ==> POST
 router.post("/add-product", adminController.postAddProduct);
 
+// admin/edit-product ==> GET
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+// admin/edit-product ==> POST
+router.post("/edit-product", adminController.postEditProduct);
+
 // admin/products ==> Get
 router.get("/products", adminController.getProducts);
 
