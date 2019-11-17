03a66f19a4f315082d3a4e33d8c28e6958556062
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2019-11-17 22:04:47 +0330

    dynamic route segment | add product to cart |detail product | work some feuture pug

---
+++ models/cart.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+const outputPath = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+const getDataFromFile = cb => {
+  fs.readFile(outputPath, (err, fileContent) => {
+    err ? cb(null) : cb(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    getDataFromFile(prevData => {
+      let cart = { products: [], totalPrice: 0 };
+
+      // Fetch the previous cart
+      prevData && (cart = prevData);
+
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(x => x.id === id);
+      const existingProduct = cart.products[existingProductIndex];
+
+      // Add new product/ increase quantity
+      if (existingProduct) {
+        cart.products[existingProductIndex] = {
+          ...existingProduct,
+          qty: existingProduct.qty + 1
+        };
+      } else {
+        cart.products = [...cart.products, { id, qty: 1 }];
+      }
+
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(outputPath, JSON.stringify(cart), err => {
+        console.log(
+          "method addProduct from Model Cart , callback writeFile",
+          err
+        );
+      });
+    });
+  }
+};
--- models/products.js
+++ models/products.js
@@ -1,39 +1,43 @@
 const fs = require("fs");
 const path = require("path");
 
+const outputPath = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getDataFromFile = cb => {
+  fs.readFile(outputPath, (err, fileContent) => {
+    err ? cb([]) : cb(JSON.parse(fileContent));
+  });
+};
+
 class Product {
   constructor(title, imageUrl, price, description) {
+    this.id = Math.random().toString();
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
     this.description = description;
   }
-  get outputPath() {
-    return path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-  }
   save() {
-    fs.readFile(this.outputPath, (err, data) => {
-      let products = [];
-      !err && (products = JSON.parse(data));
-      products.push(this);
-      fs.writeFile(this.outputPath, JSON.stringify(products), err => {
-        console.log(err);
+    getDataFromFile(fileContent => {
+      fileContent.push(this);
+      fs.writeFile(outputPath, JSON.stringify(fileContent), err => {
+        console.log("method Save Product , callback writeFile", err);
       });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, data) => {
-      !err ? cb(JSON.parse(data)) : cb([]);
+    getDataFromFile(cb);
+  }
+
+  static findByIndex(id, cb) {
+    getDataFromFile(content => {
+      let rsp = content.find(x => x.id === id);
+      rsp ? cb(false, rsp) : cb(true, rsp);
     });
   }
 }
