02b0c9228ca1597ebd4ab3b96c9b8f45381f4bed
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2019-12-01 16:08:44 +0330

    db file

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,23 +2,13 @@ const Product = require("../models/products");
 const Cart = require("../models/cart");
 
 exports.getIndex = (req, res, next) => {
-  // Product.fetchAll(product => {
-  //   res.render("shop/index", {
-  //     data: product,
-  //     title: "SHOP",
-  //     path: "/"
-  //   });
-  // });
-  Product.fetchAll()
-    .then(([rows, fields]) => {
-      // console.log(JSON.stringify(rows));
-      res.render("shop/index", {
-        data: rows,
-        title: "SHOP",
-        path: "/"
-      });
-    })
-    .catch(err => console.log(err));
+  Product.fetchAll(product => {
+    res.render("shop/index", {
+      data: product,
+      title: "SHOP",
+      path: "/"
+    });
+  });
 };
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
@@ -27,22 +17,13 @@ exports.getOrders = (req, res, next) => {
   });
 };
 exports.getProducts = (req, res, next) => {
-  // Product.fetchAll(product => {
-  //   res.render("shop/product-list", {
-  //     data: product,
-  //     title: "Products List Page",
-  //     path: "/products"
-  //   });
-  // });
-  Product.fetchAll()
-    .then(([rows, fields]) => {
-      res.render("shop/index", {
-        data: rows,
-        title: "Products List Page",
-        path: "/products"
-      });
-    })
-    .catch(err => console.log(err));
+  Product.fetchAll(product => {
+    res.render("shop/product-list", {
+      data: product,
+      title: "Products List Page",
+      path: "/products"
+    });
+  });
 };
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
