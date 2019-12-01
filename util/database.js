c5fb1f841440399fd3187babb530828e44ef9d6c
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2019-12-01 17:37:20 +0330

    setup env and Model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   // user: "mohamadreza",
+//   // password: "123",
+//   database: "node-complete"
+// });
+// module.exports = pool.promise();
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  // user: "mohamadreza",
-  // password: "123",
-  database: "node-complete"
-});
-module.exports = pool.promise();
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION
+  }
+);
+module.exports = sequelize;
