9fcb08bf68e84792fbe093b5574943395cf4a153
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-06-28 20:49:57 +0430

    prepare for rest

--- middleware/is-auth.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.session.has_login) {
-    return res.redirect("/login");
-  }
-  next();
-};
--- purenode/app.js
+++
@@ -1,4 +0,0 @@
-const http = require("http");
-const route = require("./routes");
-const server = http.createServer(route);
-server.listen(3000);
