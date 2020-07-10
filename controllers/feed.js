da889c6b96cafda4117a418e599957278685741c
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-07-10 21:42:54 +0430

    update Post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator");
+const fs = require("fs");
+const path = require("path");
 
 const Post = require("../models/post");
 const post = require("../models/post");
@@ -81,6 +83,39 @@ exports.updatePost = (req, res, next) => {
   if (req.file) {
     image = req.file.path.replace(/\\/g, "/");
   }
+  if (!image) {
+    const error = new Error("No file Picked!");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(post_id)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (image !== post.imageUrl) {
+        clearImag(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = image;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Post Update",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.getSinglePost = (req, res, next) => {
@@ -104,4 +139,11 @@ exports.getSinglePost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+const clearImag = (filePath) => {
+  // console.log("filePath", filePath);
+  filePath = path.join(__dirname, "..", filePath);
+  // console.log("filePath2", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
